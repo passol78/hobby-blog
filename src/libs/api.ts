a6523ae76e64b80a63792ff266d4bd2843e99e54
yutaka.kanno <yutaka.kanno@livesense.co.jp>
2021-04-26 12:39:49 +0900

    previewモードの対応を行った

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,7 +1,15 @@
 export const fetchApi = <T>(path: string): Promise<T> => {
-  const key = {
-    headers: {'X-API-KEY': process.env.MICROCMS_API_KEY!},
+  type RequestParams = {
+    'X-API-KEY': string
+    'X-GLOBAL-DRAFT-KEY': string
+  } | {
+    'X-API-KEY': string
+  }
+  const params: { headers: RequestParams } = {
+    headers: process.env.IS_DRAFT
+      ? {'X-API-KEY': process.env.MICROCMS_API_KEY!, "X-GLOBAL-DRAFT-KEY": process.env.MICROCMS_DRAFT_KEY! }
+      : {'X-API-KEY': process.env.MICROCMS_API_KEY!},
   };
 
-  return fetch(`${process.env.MICROCMS_HOST!}${path}`, key).then((res) => res.json())
+  return fetch(`${process.env.MICROCMS_HOST!}${path}`, params).then((res) => res.json())
 }
